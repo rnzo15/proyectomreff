6dc3ceb3b5243708e3efcdbf91de3a5d9bb27a2d
Mariano <marianohernandez2014@gmail.com>
2023-11-01 17:21:04 -0300

    Se crearon funciones para la busqueda y agregado de usuarios

--- js/scriptback.js
+++ js/scriptback.js
@@ -1,10 +1,26 @@
 
 // POPUP USUARIO
 
-function showSearchUser()  { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
-    var popup = document.getElementById("searchu");
-    popup.style.display = "block";
-}
+    function showSearchUser() {
+        var busqueda = prompt("Ingrese la cédula o el ID del usuario que desea buscar:");
+
+        if (busqueda !== null && busqueda.trim() !== "") {
+            $.ajax({
+                type: "POST",
+                url: "../src/controlador/user_search.php", // Reemplaza con la URL de tu archivo PHP
+                data: { busqueda: busqueda },
+                success: function (data) {
+                    $("#resultado").html(data);
+                },
+                error: function () {
+                    $("#resultado").html("Error en la solicitud AJAX.");
+                }
+            });
+        } else {
+            $("#resultado").html("Ingrese un valor de búsqueda válido.");
+        }
+    }
+
 
 function closePopupSearch() { //Mismo aca, cambiar nombre de la funcion y getElementById.
     var popup = document.getElementById("searchu");
@@ -40,7 +56,7 @@ function closePopupAdd() {
 // POPUP LINEA
 
 
-function showSearchLine()  { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
+function showSearchLine() { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
     let popup = document.getElementById("searchline");
     popup.style.display = "block";
 }
@@ -48,137 +64,137 @@ function closePopupSearchLine() {
     let popup = document.getElementById("searchline");
     popup.style.display = "none";
 }
-function showAddLine()  { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
+function showAddLine() { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
     let popup = document.getElementById("addLine");
     popup.style.display = "block";
 }
- function closePopupAddLine() {
- let popup = document.getElementById("addLine");
-popup.style.display = "none";
- }
-function showDelLine()  { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
- let popup = document.getElementById("delLine");
- popup.style.display = "block";
- }
- function closePopupDelLine() {
-  let popup = document.getElementById("delLine");
-popup.style.display = "none";
+function closePopupAddLine() {
+    let popup = document.getElementById("addLine");
+    popup.style.display = "none";
 }
-function showModLine()  { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
-let popup = document.getElementById("modLine");
-popup.style.display = "block";
+function showDelLine() { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
+    let popup = document.getElementById("delLine");
+    popup.style.display = "block";
+}
+function closePopupDelLine() {
+    let popup = document.getElementById("delLine");
+    popup.style.display = "none";
+}
+function showModLine() { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
+    let popup = document.getElementById("modLine");
+    popup.style.display = "block";
 }
 function closePopupModLine() {
-let popup = document.getElementById("modLine");
- popup.style.display = "none";
- }
-
- //POPUP BUS
-
-function showSearchBus()  { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
- let popup = document.getElementById("searchBus");
- popup.style.display = "block";
+    let popup = document.getElementById("modLine");
+    popup.style.display = "none";
 }
- function closePopupSearchBus() {
-let popup = document.getElementById("searchBus");
-popup.style.display = "none";
- }
 
-function showAddBus()  { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
- let popup = document.getElementById("addBus");
-popup.style.display = "block";
- }
+//POPUP BUS
+
+function showSearchBus() { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
+    let popup = document.getElementById("searchBus");
+    popup.style.display = "block";
+}
+function closePopupSearchBus() {
+    let popup = document.getElementById("searchBus");
+    popup.style.display = "none";
+}
+
+function showAddBus() { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
+    let popup = document.getElementById("addBus");
+    popup.style.display = "block";
+}
 function closePopupAddBus() {
- let popup = document.getElementById("addBus");
- popup.style.display = "none";
+    let popup = document.getElementById("addBus");
+    popup.style.display = "none";
 }
-function showDelBus()  { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
- let popup = document.getElementById("delBus");
- popup.style.display = "block";
- }
- function closePopupDelBus() {
-let popup = document.getElementById("delBus");
-popup.style.display = "none";
+function showDelBus() { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
+    let popup = document.getElementById("delBus");
+    popup.style.display = "block";
 }
-function showModBus()  { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
-let popup = document.getElementById("modBus");
-popup.style.display = "block";
+function closePopupDelBus() {
+    let popup = document.getElementById("delBus");
+    popup.style.display = "none";
+}
+function showModBus() { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
+    let popup = document.getElementById("modBus");
+    popup.style.display = "block";
 }
 
 function closePopupModBus() {
-let popup = document.getElementById("modBus");
-popup.style.display = "none";
-    }
+    let popup = document.getElementById("modBus");
+    popup.style.display = "none";
+}
 
 //POPUP PARADA
 
-function showSearchParada()  { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
-let popup = document.getElementById("searchParada");
-popup.style.display = "block";
+function showSearchParada() { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
+    let popup = document.getElementById("searchParada");
+    popup.style.display = "block";
 }
 function closePopupSearchParada() {
-let popup = document.getElementById("searchParada");
-popup.style.display = "none";
+    let popup = document.getElementById("searchParada");
+    popup.style.display = "none";
 }
-function showAddParada()  { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
-let popup = document.getElementById("addParada");
-popup.style.display = "block";
+function showAddParada() { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
+    let popup = document.getElementById("addParada");
+    popup.style.display = "block";
 }
 function closePopupAddParada() {
-let popup = document.getElementById("addParada");
-popup.style.display = "none";
+    let popup = document.getElementById("addParada");
+    popup.style.display = "none";
 }
-function showDelParada()  { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
-let popup = document.getElementById("delParada");
-popup.style.display = "block";
+function showDelParada() { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
+    let popup = document.getElementById("delParada");
+    popup.style.display = "block";
 }
 function closePopupDelParada() {
-let popup = document.getElementById("delParada");
-popup.style.display = "none";
+    let popup = document.getElementById("delParada");
+    popup.style.display = "none";
 }
-function showModParada()  { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
-let popup = document.getElementById("modParada");
-popup.style.display = "block";
+function showModParada() { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
+    let popup = document.getElementById("modParada");
+    popup.style.display = "block";
 }
 
 function closePopupModParada() {
-let popup = document.getElementById("modParada");
-popup.style.display = "none";
+    let popup = document.getElementById("modParada");
+    popup.style.display = "none";
 }
 
 //POPUP TARIFA
 
-function showSearchTarifa()  { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
-let popup = document.getElementById("searchTarifa");
-popup.style.display = "block";
+function showSearchTarifa() { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
+    let popup = document.getElementById("searchTarifa");
+    popup.style.display = "block";
 }
 function closePopupSearchTarifa() {
-let popup = document.getElementById("searchTarifa");
-popup.style.display = "none";
+    let popup = document.getElementById("searchTarifa");
+    popup.style.display = "none";
 }
-function showAddTarifa()  { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
-let popup = document.getElementById("addTarifa");
-popup.style.display = "block";
+function showAddTarifa() { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
+    let popup = document.getElementById("addTarifa");
+    popup.style.display = "block";
 }
 function closePopupAddTarifa() {
-let popup = document.getElementById("addTarifa");
-popup.style.display = "none";
+    let popup = document.getElementById("addTarifa");
+    popup.style.display = "none";
 }
-function showDelTarifa()  { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
-let popup = document.getElementById("delTarifa");
-popup.style.display = "block";
+function showDelTarifa() { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
+    let popup = document.getElementById("delTarifa");
+    popup.style.display = "block";
 }
 function closePopupDelTarifa() {
-let popup = document.getElementById("delTarifa");
-popup.style.display = "none";
+    let popup = document.getElementById("delTarifa");
+    popup.style.display = "none";
 }
-function showModTarifa()  { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
-let popup = document.getElementById("modTarifa");
-popup.style.display = "block";
+function showModTarifa() { //Para crear otro popup, crear una funcion nueva, cambiandole el nombre y el getElementById
+    let popup = document.getElementById("modTarifa");
+    popup.style.display = "block";
 }
 function closePopupModTarifa() {
-let popup = document.getElementById("modTarifa");
-popup.style.display = "none";
+    let popup = document.getElementById("modTarifa");
+    popup.style.display = "none";
 }
 
 //POPUP CIUDAD
@@ -188,57 +204,106 @@ function showSearchCiudad() {
     popup.style.display = "block";
 }
 function closePopupSearchCiudad() {
-let popup = document.getElementById("searchCiudad");
-popup.style.display = "none";
+    let popup = document.getElementById("searchCiudad");
+    popup.style.display = "none";
 }
 function showAddCiudad() {
     let popup = document.getElementById("addCiudad");
     popup.style.display = "block";
 }
 function closePopupAddCiudad() {
-let popup = document.getElementById("addCiudad");
-popup.style.display = "none";
+    let popup = document.getElementById("addCiudad");
+    popup.style.display = "none";
 }
 function showDelCiudad() {
     let popup = document.getElementById("delCiudad");
     popup.style.display = "block";
 }
 function closePopupDelCiudad() {
-let popup = document.getElementById("delCiudad");
-popup.style.display = "none";
+    let popup = document.getElementById("delCiudad");
+    popup.style.display = "none";
 }
 function showModCiudad() {
-let popup = document.getElementById("modCiudad");
-popup.style.display = "block";
+    let popup = document.getElementById("modCiudad");
+    popup.style.display = "block";
 }
 function closePopupModCiudad() {
-let popup = document.getElementById("modCiudad");
-popup.style.display = "none";
- }
+    let popup = document.getElementById("modCiudad");
+    popup.style.display = "none";
+}
 
 //POPUP HORARIO
 
- function showAddHorario() {
-let popup = document.getElementById("addHorario");
-popup.style.display = "block";
+function showAddHorario() {
+    let popup = document.getElementById("addHorario");
+    popup.style.display = "block";
 }
 function closePopupAddHorario() {
-let popup = document.getElementById("addHorario");
-popup.style.display = "none";
- }
- function showDelHorario() {
+    let popup = document.getElementById("addHorario");
+    popup.style.display = "none";
+}
+function showDelHorario() {
     let popup = document.getElementById("delHorario");
     popup.style.display = "block";
-    }
+}
 function closePopupDelHorario() {
-let popup = document.getElementById("delHorario");
-popup.style.display = "none";
+    let popup = document.getElementById("delHorario");
+    popup.style.display = "none";
 }
 function showModHorario() {
-let popup = document.getElementById("modHorario");
-popup.style.display = "block";
+    let popup = document.getElementById("modHorario");
+    popup.style.display = "block";
 }
 function closePopupModHorario() {
-let popup = document.getElementById("modHorario");
-popup.style.display = "none";
+    let popup = document.getElementById("modHorario");
+    popup.style.display = "none";
 }
+
+$(document).ready(function () {
+    $("#buscar").click(function () {
+        var busqueda = $("#busqueda").val();
+        if (busqueda) {
+            $.ajax({
+                type: "POST",
+                url: "../src/controlador/user_search.php", // Reemplaza con la URL de tu archivo PHP
+                data: { busqueda: busqueda },
+                success: function (data) {
+                    $("#resultado").html(data);
+                },
+                error: function () {
+                    $("#resultado").html("Error en la solicitud AJAX.");
+                }
+            });
+        } else {
+            $("#resultado").html("Ingrese un valor de búsqueda.");
+        }
+    });
+});
+
+$(document).ready(function () {
+    $("#agregar-usuario-form").submit(function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            type: "POST",
+            url: "../src/controlador/user_add.php",
+            data: $(this).serialize(),
+            dataType: 'json', // Especifica que se espera un JSON como respuesta
+            success: function (data) {
+                if (data.error) {
+                    // Muestra el mensaje de error
+                    $("#mensaje").html(data.error);
+                } else {
+                    // Muestra el mensaje de éxito y los datos del usuario
+                    $("#mensaje").html("Usuario agregado con éxito.<br>" +
+                        "ID: " + data.iduser + "<br>" +
+                        "Nombre de usuario: " + data.username + "<br>" +
+                        "Correo electrónico: " + data.email);
+                }
+            },
+            error: function () {
+                $("#mensaje").html("Error en la solicitud AJAX.");
+            }
+        });
+    });
+});

15adf35558cf579b939b986ad661430477c3b43b
Mariano <marianohernandez2014@gmail.com>
2023-11-01 17:23:28 -0300

    Se creo la conexion externa a la base de datos, con datos de mi PC

--- src/libs/Conexion.php
+++ src/libs/Conexion.php
@@ -1,49 +1,13 @@
 <?php
+$servername = "localhost";
+$usernameDB = "root";
+$passwordDB = "6751";
+$database = "FBUSESUY";
 
-namespace Busesuy\Bsapp\libs;
-
-
-use PDO;
-use PDOException;
-
-class Conexion
-{
-  private static $conexion;
-  private $pdo;
-
-  private function __construct()
-  {
-    try {
-      $host     = constant('HOST');
-      $port     = constant('PORT_DB');
-      $db       = constant('DB');
-      $user     = constant('USER');
-      $password = constant('PASSWORD');
-      $charset  = constant('CHARSET');
-      $connection = "mysql:host={$host};port={$port};dbname={$db};charset={$charset}";
-      $options    = [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_EMULATE_PREPARES => false,
-      ];
-      $this->pdo = new PDO($connection, $user, $password, $options);
-    } catch (PDOException $e) {
-      print_r('Error connection: ' . $e->getMessage());
-    }
-  }
-
-  public static function getConexion()
-  {
-    if (!isset(self::$conexion)) {
-      self::$conexion = new Conexion();
-    }
-    return self::$conexion;
-  }
-
-  /**
-   * Get the value of pdo
-   */
-  public function getPdo()
-  {
-    return $this->pdo;
-  }
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$database", $usernameDB, $passwordDB);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  echo "Error de conexión a la base de datos: " . $e->getMessage();
 }
+?>
